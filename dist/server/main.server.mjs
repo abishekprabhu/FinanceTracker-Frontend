7269fb9e966f5caf0691a2f99b0412c47cc39871
Abishek Prabhu <abiskanna@gmail.com>
2025-02-24 18:00:01 +0530

    try again for deploy

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{c as a}from"./chunk-22YT45PB.mjs";import"./chunk-23ZGX32M.mjs";import"./chunk-DQT66YTT.mjs";import"./chunk-O2GR7IF5.mjs";import"./chunk-IBAMZZJY.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-7TAQHURJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{c as a}from"./chunk-IVIHO5M4.mjs";import"./chunk-23ZGX32M.mjs";import"./chunk-DQT66YTT.mjs";import"./chunk-O2GR7IF5.mjs";import"./chunk-IBAMZZJY.mjs";import"./chunk-UYFIRDFP.mjs";import"./chunk-7TAQHURJ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, OnInit, PLATFORM_ID } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'FinanceTracker-Frontend';
+  private isBrowser: boolean;
+
+  constructor(
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.isBrowser = isPlatformBrowser(this.platformId); // Check if it's the browser
+  }
+
+  ngOnInit(): void {
+    console.log('Platform:', this.platformId);
+    if (this.isBrowser) {
+      console.log('Running in browser');
+      this.document.body.classList.add('dark'); // This will only run in the browser
+    } else {
+      console.log('Running on server');
+    }
+  }
 }
